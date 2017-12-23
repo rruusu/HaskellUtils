dad6290eee3a6301ee7017df493c397047f9adfe
Reino Ruusu <rruusu@yahoo.co.uk>
2017-12-24 00:12:13 +0200

    Some additions to Utils.hs.

--- Utils.hs
+++ Utils.hs
@@ -4,6 +4,20 @@
 
 module Utils where
 
--- Functional if
+-- Functional if: if' c a b = if c then a else b
 if' True x _ = x
 if' False _ y = y
+
+-- Left-binding low-precedence function application:
+-- map & replaceWithWhen (const z) (<0) $ zipWith (-) & getx pts & gety pts
+infixl 1 &
+(&) :: (a -> b) -> a -> b
+(&) = ($)
+
+-- Very nice for conditional processing
+replaceWithWhen :: (a -> a) -> (a -> Bool) -> a -> a
+replaceWithWhen f b x = if' (b x) (f x) x
+
+-- Very nice for conditional processing
+replaceWithIf :: (a -> a) -> Bool -> a -> a
+replaceWithIf f b x = if' b (f x) x
